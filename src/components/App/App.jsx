cf1117afb3e381deb07169f4d1e88c66e127cddd
Parselledev <parselledev@gmail.com>
2022-02-27 20:54:36 +0200

    added emotion

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import Header from 'components/Header';
+import Content from 'components/Content';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Content />
     </div>
   );
 };
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,33 @@
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
 const Header = () => {
-  return <div></div>;
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Test place
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
